cdb5809b9e4ad19aa93b16683a5af94b1dfb540a
RameshBettge <bettge.r@gmx.de>
2018-08-11 14:04:04 +0200

    Added: Unity Scene. Placeholder Sprites. Unfinished Script

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    bool inGame = false;
+
+    Vector2 inputVector;
+    Vector2 moveVector;
+
+    float inputSqrMagnitude;
+    float inputDeadzone = 0.2f;
+
+    private void Update()
+    {
+        //if (!inGame && inputVector.sqrMagnitude)
+        //{
+
+        //}   
+
+        GetInput();
+
+    }
+
+    private void GetInput()
+    {
+        inputVector.x = Input.GetAxis("Horizontal");
+        inputVector.y = Input.GetAxis("Vertical");
+
+        inputSqrMagnitude = inputVector.sqrMagnitude;
+
+        //moveVector 
+    }
+}
